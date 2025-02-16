fdbcdd838f219ff199227d084e18c90a00a66089
Ryan Spain <8723433+ryanspain@users.noreply.github.com>
2025-02-16 21:12:00 +0000

    adds automated test for copy as page link using Playwright

---
+++ playwright.config.js
@@ -0,0 +1,34 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: './test/specs',
+  outputDir: './test/results',
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [['html', { outputFolder: 'test/report', open: 'never' }]],
+  use: {
+    trace: 'on',
+  },
+  projects: [
+    {
+      name: 'Chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'Firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    // Test against branded browsers
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    },
+    {
+      name: 'Microsoft Edge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    },
+  ],
+});
