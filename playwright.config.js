ffb933f4ee54507df7197fd99864392b63d1a621
Ryan Spain <8723433+ryanspain@users.noreply.github.com>
2025-10-16 19:30:28 +0100

    Refactor reporter configuration for improved readability in Playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -7,7 +7,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? 'github' : [['html', { outputFolder: 'test/report', open: 'never' }]],
+  reporter: [
+    ['html', { outputFolder: 'test/report', open: 'never' }],
+    ...(process.env.CI ? [['github']] : []),
+  ],
   use: {
     trace: 'on',
   },
